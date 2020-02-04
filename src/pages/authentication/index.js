65506eb165de27758c9b2642b2c9c93bcdb0a301
vladimir <lovkiy2012@gmail.com>
2020-02-04 18:17:27 +0300

    7 login

---
+++ src/pages/authentication/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Authentication = () => {
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Login</h1>
+            <p className="text-xs-center">
+              <Link to="register">Need an account?</Link>
+            </p>
+            <form action="">
+              <fieldset>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="email"
+                    placeholder="Email"
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="password"
+                    placeholder="Password"
+                  />
+                </fieldset>
+                <button
+                  className="btn btn-primary btn-lg pull-xs-right"
+                  type="submit"
+                >
+                  Sign in
+                </button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Authentication;
