684b076122fe1a9da44816c2a1fd42b59ecc2b55
vladimir <lovkiy2012@gmail.com>
2020-02-05 21:29:07 +0300

    15 pageRegistration

--- src/pages/authentication/index.js
+++ src/pages/authentication/index.js
@@ -1,46 +1,56 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Axios from "axios";
 
-const Authentication = () => {
+import useFetch from "../../useFetch/useFetch";
+
+const Authentication = props => {
+  const isLogin = props.match.path === "/login";
+  const pageTitle = isLogin ? "Sign in" : "Sign up";
+  const descriptionLink = isLogin ? "/register" : "/login";
+  const descriptionText = isLogin ? "Need an account?" : "Have an account?";
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isSubmiting, setSubmiting] = useState(false);
-  //   const [{response,isLoading,error,doFetch}]=useFetch('someUrl')
+  const apiUrl = isLogin ? "/users/login" : "/users";
+  const [{ isLoading, response, error }, doFetch] = useFetch(apiUrl);
+
+  console.log(isLogin);
   const hundleSubmit = e => {
     e.preventDefault();
-    setSubmiting(true);
-  };
-  useEffect(() => {
-    console.log("useEffectTrigger");
-    document.title = email;
-    if (!isSubmiting) {
-      return;
-    }
-    Axios("https://conduit.productionready.io/api/users/login", {
+   
+    const user = isLogin ? { email, password } : { email, password, username };
+    console.log(user)
+    doFetch({
       method: "post",
-      data: { user: { email: "sds@ssxs.ww", password: "123" } }
-    })
-      .then(response => {
-        console.log(response);
-        setSubmiting(false);
-      })
-      .catch(error => {
-        setSubmiting(false);
-        console.log(error);
-      });
-  }, [isSubmiting]);
+      data: {
+        user
+      }
+    });
+  };
+
   return (
     <div className="auth-page">
       <div className="container page">
         <div className="row">
           <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Login</h1>
+            <h1 className="text-xs-center">{pageTitle}</h1>
             <p className="text-xs-center">
-              <Link to="register">Need an account?</Link>
+              <Link to={descriptionLink}>{descriptionText}</Link>
             </p>
             <form onSubmit={hundleSubmit}>
               <fieldset>
+                {!isLogin && (
+                  <fieldset className="form-group">
+                    <input
+                      className="form-control form-control-lg"
+                      type="text"
+                      placeholder="Username"
+                      value={username}
+                      onChange={e => setUsername(e.target.value)}
+                    />
+                  </fieldset>
+                )}
+
                 <fieldset className="form-group">
                   <input
                     className="form-control form-control-lg"
@@ -62,9 +72,9 @@ const Authentication = () => {
                 <button
                   className="btn btn-primary btn-lg pull-xs-right"
                   type="submit"
-                  disabled={isSubmiting}
+                  disabled={isLoading}
                 >
-                  Sign in
+                  {pageTitle}
                 </button>
               </fieldset>
             </form>
---
+++ src/useFetch/useFetch.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import Axios from "axios";
+const useFetch = url => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [options,setOptions]=useState({})
+  const baseUrl = "https://conduit.productionready.io/api";
+  const doFetch = (options={}) => {
+    setOptions(options)
+    setIsLoading(true)
+  };
+  useEffect(() => {
+    console.log("useEffectTrigger");
+
+    if (!isLoading) {
+      return;
+    }
+    Axios(baseUrl + url,options)
+      .then(res => {
+        console.log(response);
+        setResponse(res.data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setError(error.response.data);
+        setIsLoading(false);
+        console.log(error);
+      });
+  }, [isLoading]);
+
+  return [{ isLoading, response, error }, doFetch];
+};
+export default useFetch;
