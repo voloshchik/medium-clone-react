019b78713a694e695b765a8af05017b5f5aa6238
vladimir <lovkiy2012@gmail.com>
2020-02-10 21:17:04 +0300

    24 feed start

--- src/pages/globalFeed.js
+++ src/pages/globalFeed.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Feed from "../components/feed/feed";
+import useFetch from "../hooks/useFetch";
 
 const GlobalFeed = () => {
-    return (
-        <div>
-            GlobalFeed
+  const apiUrl = "/articles?limit=10&offset=0";
+  const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl);
+  console.log(response);
+  useEffect(() => {
+    doFetch();
+  }, [doFetch]);
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1>Medium clone</h1>
+          <p>A place to share knowledge</p>
         </div>
-    )
-}
+      </div>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9"></div>
+          <div className="col-md-3"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default GlobalFeed
+export default GlobalFeed;
