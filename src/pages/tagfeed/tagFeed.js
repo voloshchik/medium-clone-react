4a1c7c70b3b2be9772c2b0ba3817f18d93d4be9a
vladimir <lovkiy2012@gmail.com>
2020-02-13 21:40:18 +0300

    28

--- src/pages/tagfeed/tagFeed.js
+++ src/pages/tagfeed/tagFeed.js
@@ -4,19 +4,19 @@ import Feed from "../../components/feed/feed";
 import useFetch from "../../hooks/useFetch";
 import Pagination from "../../components/pagination/pagination";
 import { getPagination, limit } from "../../utils";
-import PopularTags from '../../components/pupularTags/popularTags';
+import PopularTags from "../../components/pupularTags/popularTags";
 import Loading from "../../components/loading/loading";
-import ErrorMessage from '../../components/errorMessage/errorMessage';
-import FeedToggler from '../../components/feedToggler/feedToggler';
+import ErrorMessage from "../../components/errorMessage/errorMessage";
+import FeedToggler from "../../components/feedToggler/feedToggler";
 
 const TagFeed = ({ location, match }) => {
   const [currentPage, offset] = getPagination(location.search);
-  const tagName=match.params.slug
+  const tagName = match.params.slug;
   console.log("fff", currentPage, offset);
   const stringifiedParams = stringify({
     limit,
     offset,
-    tag:tagName
+    tag: tagName
   });
   const apiUrl = `/articles?${stringifiedParams}`;
   const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl);
@@ -24,7 +24,7 @@ const TagFeed = ({ location, match }) => {
   const url = match.url;
   useEffect(() => {
     doFetch();
-  }, [doFetch, currentPage,tagName]);
+  }, [doFetch, currentPage, tagName]);
   return (
     <div className="home-page">
       <div className="banner">
@@ -36,9 +36,9 @@ const TagFeed = ({ location, match }) => {
       <div className="container page">
         <div className="row">
           <div className="col-md-9">
-            <FeedToggler />
-            {isLoading && <Loading/>}
-            {error && <ErrorMessage/>}
+            <FeedToggler tagName={tagName} />
+            {isLoading && <Loading />}
+            {error && <ErrorMessage />}
             {!isLoading && response && (
               <>
                 <Feed articles={response.articles} />
@@ -51,7 +51,9 @@ const TagFeed = ({ location, match }) => {
               </>
             )}
           </div>
-          <div className="col-md-3"><PopularTags/></div>
+          <div className="col-md-3">
+            <PopularTags />
+          </div>
         </div>
       </div>
     </div>
