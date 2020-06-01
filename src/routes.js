1db1aa7007d99f835d223ecf0f1f317ce9d93101
vladimir <lovkiy2012@gmail.com>
2020-06-01 18:17:16 +0300

    37 user-profile-page

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,23 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import GlobalFeed from "./pages/globalFeed";
-import Article from "./pages/article";
-import Authentication from "./pages/authentication";
-import TagFeed from "./pages/tagfeed/tagFeed";
-import YouFeed from "./pages/youFeed/youFeed";
-import CreateArticle from "./components/CreateArticle/CreateArticle";
-import EditArticle from "./pages/EditArticle/EditArticle";
-import Settings from "./pages/Settings/Settings";
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import GlobalFeed from './pages/globalFeed'
+import Article from './pages/article'
+import Authentication from './pages/authentication'
+import TagFeed from './pages/tagfeed/tagFeed'
+import YouFeed from './pages/youFeed/youFeed'
+import CreateArticle from './components/CreateArticle/CreateArticle'
+import EditArticle from './pages/EditArticle/EditArticle'
+import Settings from './pages/Settings/Settings'
+import UserProfile from './pages/userProfile'
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/" component={GlobalFeed} exact />
       <Route path="/settings" component={Settings} />
+      <Route path="/profiles/:slug" component={UserProfile} />
+      <Route path="/profiles/:slug/favorites" component={UserProfile} />
+
       <Route path="/articles/new" component={CreateArticle} />
       <Route path="/articles/:slug/edit" component={EditArticle} />
       <Route path="/feed" component={YouFeed} />
@@ -22,6 +26,6 @@ const Routes = () => {
       <Route path="/login" component={Authentication} />
       <Route path="/register" component={Authentication} />
     </Switch>
-  );
-};
-export default Routes;
+  )
+}
+export default Routes
