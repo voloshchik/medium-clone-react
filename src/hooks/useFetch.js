f86db115ea317690f69be082d1ccc54b2c6ed39c
vladimir <lovkiy2012@gmail.com>
2020-06-02 18:15:06 +0300

    39 destroy useEffect callback

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,21 +1,22 @@
-import { useState, useEffect, useCallback } from "react";
-import Axios from "axios";
-import useLocalStorage from "./useLocalStorage";
+import {useState, useEffect, useCallback} from 'react'
+import Axios from 'axios'
+import useLocalStorage from './useLocalStorage'
 
-const useFetch = url => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [response, setResponse] = useState(null);
-  const [error, setError] = useState(null);
-  const [options, setOptions] = useState({});
-  const [token] = useLocalStorage("token");
-  const baseUrl = "https://conduit.productionready.io/api";
+const useFetch = (url) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [response, setResponse] = useState(null)
+  const [error, setError] = useState(null)
+  const [options, setOptions] = useState({})
+  const [token] = useLocalStorage('token')
+  const baseUrl = 'https://conduit.productionready.io/api'
 
   const doFetch = useCallback((options = {}) => {
-    setOptions(options);
-    setIsLoading(true);
-  }, []);
+    setOptions(options)
+    setIsLoading(true)
+  }, [])
 
   useEffect(() => {
+    let skipGetResponseAfterDesroy = false
     if (!isLoading) {
       return
     }
@@ -24,22 +25,28 @@ const useFetch = url => {
       ...options,
       ...{
         headers: {
-          authorization: token ? `Token ${token}` : ''
-        }
-      }
+          authorization: token ? `Token ${token}` : '',
+        },
+      },
     }
     Axios(baseUrl + url, requestOptions)
-      .then(res => {
-        
-        setResponse(res.data);
-        setIsLoading(false);
+      .then((res) => {
+        if (!skipGetResponseAfterDesroy) {
+          setResponse(res.data)
+          setIsLoading(false)
+        }
       })
-      .catch(error => {
-        setError(error.response.data);
-        setIsLoading(false);
-      });
-  }, [isLoading, url, options, token]);
+      .catch((error) => {
+        if (!skipGetResponseAfterDesroy) {
+          setError(error.response.data)
+          setIsLoading(false)
+        }
+      })
+    return () => {
+      skipGetResponseAfterDesroy = true
+    }
+  }, [isLoading, url, options, token])
 
-  return [{ isLoading, response, error }, doFetch];
-};
-export default useFetch;
+  return [{isLoading, response, error}, doFetch]
+}
+export default useFetch
